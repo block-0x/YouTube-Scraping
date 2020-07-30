5f268a960a91a499add003bacd73e820e203371c
“maki5588” <your-mail@example.com>
2020-07-31 02:36:25 +0900

    create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+from extraction import YoutubeChannelVideoScraper
+from mean import Functions
+
+# YoutubeChannelVideoScraper
+scraper = YoutubeChannelVideoScraper()
+scraper.run()
+# Functions
+function = Functions()
+function.run()
