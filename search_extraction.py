7e9f88b2bfc4f11a9ce12dc2994bbda76b7db050
“maki5588” <your-mail@example.com>
2020-08-01 16:22:36 +0900

    Duplicate deletion

--- search_extraction.py
+++ search_extraction.py
@@ -16,7 +16,7 @@ class YouTubeSearchScraper(object):
         '''
         youtube_url
         '''
-        self.search_query = "検索ワード"
+        self.search_query = "asmr"
         self.youtube_url = "https://www.youtube.com/"
         self.search_url = "results?search_query="
         '''
@@ -24,8 +24,8 @@ class YouTubeSearchScraper(object):
         '''
         self.search_data_csv_file_name = "./data/youtube_search_raw_data"
         self.channel_list_csv_file_name = "./data/youtube_channel_list"
-        self.csv_file_path = os.path.join(os.getcwd(), self.search_data_csv_file_name+'.csv')
-        self.csv_file_path = os.path.join(os.getcwd(), self.channel_list_csv_file_name+'.csv')
+        self.search_data_csv_file_path = os.path.join(os.getcwd(), self.search_data_csv_file_name+'.csv')
+        self.channel_list_csv_file_path = os.path.join(os.getcwd(), self.channel_list_csv_file_name+'.csv')
         self.search_video_url = os.path.join(self.youtube_url, self.search_url, self.search_query)
         '''
         extraction_data
@@ -44,6 +44,7 @@ class YouTubeSearchScraper(object):
         self.parse_youtube_search_information()
         self.search_data_save_as_csv_file()
         self.channel_list_save_as_csv_file()
+        self.driver.close()
 
 
     def get_page_source(self):
@@ -65,8 +66,13 @@ class YouTubeSearchScraper(object):
             html = self.driver.page_source
             if self.current_html != html:
                 self.current_html=html
-            else:
+                t = 0
+                start = time.time()
+                t = time.time() - start
+                t == 20
                 break
+            # else:
+            #     break
 
 
     def parse_youtube_search_information(self):
@@ -158,17 +164,23 @@ class YouTubeSearchScraper(object):
          "video_time": self.video_times,
          "create_stamp": self.create_stamps
         }
-        pd.DataFrame(data).to_csv(self.csv_file_path,index=False)
-        self.driver.close()
+        print(self.channel_names)
+        pd.DataFrame(data).to_csv(self.search_data_csv_file_path,index=False)
+        # self.driver.close()
 
 
     def channel_list_save_as_csv_file(self):
+        '''
+        Duplicate deletion
+        '''
+        channel_urls = (list(set(self.channel_urls)))
+        channel_names = (list(set(self.channel_names)))
         data = {
-         "channel_url": self.channel_urls,
-         "channel_name": self.channel_names,
+         "channel_url": channel_urls,
+         "channel_name": channel_names
         }
-        pd.DataFrame(data).to_csv(self.csv_file_path,index=False)
-        self.driver.close()
+        pd.DataFrame(data).to_csv(self.channel_list_csv_file_path,index=False)
+        
 
 
 if __name__ == "__main__":
