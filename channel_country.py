88f951ccceb160dda0ec08e5d1b1fe0fcfb15b4f
“maki5588” <your-mail@example.com>
2020-08-05 08:58:24 +0900

    function done

--- channel_country.py
+++ channel_country.py
@@ -11,19 +11,26 @@ try:
     import urlparse
 except ImportError:
     import urllib.parse as urlparse
+'''
+pip install regex
+'''
+import regex
 
 
 class ChannelCountryScraper(object):
 
     def __init__(self):
         self.channel_about_urls = []
-        self.channel_countries = []
-        self.channel_subscribers = []
+        # self.channel_countries = []
+        # self.channel_subscribers = []
+        self.nihongo_channel_countries = []
+        self.channel_subscribers_true = []
 
 
     def run(self):
         self.read_youtube_urls()
         self.get_page_source()
+        # self.country_nihongo_true()
         self.channel_country_additional()
         self.channel_subscriber_additional()
         self.driver.close()
@@ -60,16 +67,19 @@ class ChannelCountryScraper(object):
                     self.current_html=html
                 else:
                     self.parse_channel_country()
+                    self.country_nihongo_true()
+                    self.channel_subscriber_set()
                     break
 
 
     def parse_channel_country(self):
+        self.channel_countries = []
+        self.channel_subscribers = []
         soup = BeautifulSoup(self.current_html, 'html.parser')
         '''
         channelSubscriberOfIntExtractionFunction
         '''
         channel_subscriber_i = soup.find("yt-formatted-string", class_="style-scope ytd-c4-tabbed-header-renderer")
-        print(channel_subscriber_i)
         channel_subscriber_lstrip = str(channel_subscriber_i).lstrip('<yt-formatted-string class="style-scope ytd-c4-tabbed-header-renderer" id="subscriber-count">')
         channel_subscriber_rstrip = channel_subscriber_lstrip.rstrip('</yt-formatted-string>')
         if "万" in channel_subscriber_rstrip:
@@ -78,20 +88,28 @@ class ChannelCountryScraper(object):
             channel_subscriber_add_million = channel_subscriber_sub + '00'
             channel_subscriber_material = int(channel_subscriber_add_million)
         elif "!--css-build:sh" in channel_subscriber_rstrip:
-            print(channel_subscriber_rstrip)
             channel_subscriber_material = None
         else:
             channel_subscriber_replace = channel_subscriber_rstrip.replace(' ', '')
             channel_subscriber_sub = re.sub("\\D", "", str(channel_subscriber_replace))
-            print(channel_subscriber_sub)
             channel_subscriber_material = int(channel_subscriber_sub)
             '''
-            CuntryOfIntExtractionFunction
+            countryOfIntExtractionFunction
             '''
         for i in soup.find_all("td", class_="style-scope ytd-channel-about-metadata-renderer"):
-            cuntry_i_findall = re.findall('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">.*</yt-formatted-string>', str(i))
-            cuntry_i_replace = str(cuntry_i_findall).replace('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">', '').replace('</yt-formatted-string>', '')
-            cuntry = str(cuntry_i_replace).replace("['", '').replace("']", '')
+            # i_x = ('{0}:{1}'.format(i, td_i[i]))
+            country_i_findall = re.findall('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">.*</yt-formatted-string>', str(i))
+            country_i_replace = str(country_i_findall).replace('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">', '').replace('</yt-formatted-string>', '')
+            country = str(country_i_replace).replace("['", '').replace("']", '')
+            # print(country)
+            # print(country)
+            # print(country)
+            # country_item = country[i]
+            # print(country_item)
+            # country_index = ('{0}:{1}'.format(i, country))
+            # print(country_index)
+            # i_x = ('{0}:{1}'.format(i, td_i[i]))
+            # print(country)
             '''
             channelSubscriberOfIntExtractionFunction
             '''
@@ -99,32 +117,82 @@ class ChannelCountryScraper(object):
             '''
             NoneExclusion
             '''
-            if "<" in cuntry:
-                cuntry = None
-            if "[]" in str(cuntry):
-                cuntry = None
-            if cuntry is None:
-                continue
-            if str(cuntry):
-                print(cuntry)
-                print(channel_subscriber)
-                self.channel_countries.append(cuntry)
-                self.channel_subscribers.append(channel_subscriber)
+            if "<!--css-build:shady-->" in str(country):
+                country = None
+            if "<" in str(country):
+                country = None
+            if "[]" in str(country):
+                country = None
+            # if country is None:
+            #     continue
+            # if False in country:
+            #     country = None
+            # if str(country).isnumeric():
+            #     country += country
+            # if country == 0:
+            #     country = None
+            # if  is country:
+            #     country = None
+            # print(country)
+            self.channel_countries.append(country)
+            # self.country_nihongo_true()
+            self.channel_subscribers.append(channel_subscriber)
+
+
+    def country_nihongo_true(self):
+        # print(self.channel_countries)
+        country_list = self.channel_countries
+        # print(country_list)
+        # print(country_list)
+        # print
+        country_list_join = ','.join(str(country_list))
+        country_list_join_replace = country_list_join.replace(',', '')
+        # country_list_str_replace_list = (list(country_list_str_replace))
+        # country_list_str_replace_findall = (re.findall('[ぁ-ゟ]+', str(country_list_str_replace_list)))
+        # print(country_list_str_replace_findall)
+        # print(country_list_str_replace)
+        # print(('%s' % country_list_str_replace))
+        # p = regex.compile(r'\p{Script_Extensions=Han}+')
+        # print(p.fullmatch("NoneNone日本"))
+        nihongo = regex.compile('[\u2E80-\u2FDF\u3005-\u3007\u3400-\u4DBF\u4E00-\u9FFF\uF900-\uFAFF\U00020000-\U0002EBEF]+')
+        country = (nihongo.findall(str(country_list_join_replace)))
+        # print(country_list_join)
+        if "[]" in str(country):
+            country = '非表示'
+        if "[" in str(country):
+            country = str(country).replace("['", '').replace("']", '')
+        # if "[" in str(country):
+        #     country = ','.join(country).replace(',', '')
+        # country
+        self.nihongo_channel_countries.append(str(country))
+        # print(self.nihongo_channel_countries)
+        # print
+
+
+    def channel_subscriber_set(self):
+        subscriber_list =  self.channel_subscribers
+        # subscriber_list_join = ','.join(str(subscriber_list))
+        # subscriber_list_join_replace = subscriber_list_join.replace(',', '')
+        subscriber = (list(set(subscriber_list)))
+        # print(subscriber)
+        # if "[]" in str(subscriber):
+        #     subscriber = 0
+        subscriber = str(subscriber).replace("[", '').replace("]", '')
+        self.channel_subscribers_true.append(subscriber)
 
 
 
     def channel_country_additional(self):
+        # print(str(self.channel_countries))
         df = pd.read_csv('./data/youtube_channel_list.csv')
-        df['channel_country'] = self.channel_countries
+        df['channel_country'] = self.nihongo_channel_countries
         pd.DataFrame(df).to_csv('./data/youtube_channel_list.csv',index=False)
 
 
     def channel_subscriber_additional(self):
-        print(self.channel_subscribers)
         df = pd.read_csv('./data/youtube_channel_list.csv')
-        df['channel_subscriber'] = self.channel_subscribers
+        df['channel_subscriber'] = self.channel_subscribers_true
         pd.DataFrame(df).to_csv('./data/youtube_channel_list.csv',index=False)
-        print(df)
 
 
 if __name__ == "__main__":
