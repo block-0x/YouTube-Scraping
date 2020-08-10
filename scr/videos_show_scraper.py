72602955ade4e567ce17df57ef8b2c06d672b06c
“maki5588” <your-mail@example.com>
2020-08-11 00:45:18 +0900

    set_default_proxy

--- scr/videos_show_scraper.py
+++ scr/videos_show_scraper.py
@@ -5,22 +5,23 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.proxy import Proxy, ProxyType
 from selenium.webdriver.common.keys import Keys
 import re
 import numpy as np
 import regex
+import json
+import requests
 try:
     import urlparse
 except ImportError:
     import urllib.parse as urlparse
-import requests
+import urllib.request, urllib.error
 '''
 proxy
 '''
 import socks, socket
 
-socks.set_default_proxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050)
-socket.socket = socks.socksocket
 
 class ChannelCountryAndScraper(object):
 
@@ -36,8 +37,10 @@ class ChannelCountryAndScraper(object):
 
     def run(self):
         self.copy_csv()
-        # self.get_page_source()
-        # self.channel_country_subscriber_add_as_csv_file()
+        self.csv_file_drop_duplicate()
+        self.read_csv_urls()
+        self.get_page_source()
+        self.parse_video_show()
         # self.csv_file_drop_duplicate()
         # self.driver.close()
 
@@ -46,7 +49,6 @@ class ChannelCountryAndScraper(object):
         df = pd.read_csv(self.channel_list_csv_file_path)
         pd.DataFrame(df).to_csv(self.scarch_videos_list_csv_file_path,index=False)
         print(self.scarch_videos_list_csv_file_path+"にコピーしました")
-        self.csv_file_drop_duplicate()
 
 
     def csv_file_drop_duplicate(self):
@@ -54,99 +56,54 @@ class ChannelCountryAndScraper(object):
         df_drop_duplicate = df.drop_duplicates(subset='video_url', keep='last')
         self.df_add_csv = pd.DataFrame(df_drop_duplicate).to_csv(self.scarch_videos_list_csv_file_path,index=False)
         print(self.scarch_videos_list_csv_file_path+"重複動画削除")
-        self.read_csv_urls()
 
 
     def read_csv_urls(self):
-            df = pd.read_csv(self.scarch_videos_list_csv_file_path,index_col='video_url')
-            video_url_data = df.index.values
-            video_urls = video_url_data.tolist()
-            for i in video_urls:
-                youtube_url = 'https://www.youtube.com'
-                self.video_url = ('%s' % i)
-                video_url_path = urlparse.urljoin(youtube_url, self.video_url)
-                self.video_urls.append(video_url_path)
+        df = pd.read_csv(self.scarch_videos_list_csv_file_path,index_col='video_url')
+        video_url_data = df.index.values
+        video_urls = video_url_data.tolist()
+        for i in video_urls:
+            youtube_url = 'https://www.youtube.com'
+            self.video_url = ('%s' % i)
+            video_url_path = urlparse.urljoin(youtube_url, self.video_url)
+            self.video_urls.append(video_url_path)
 
 
     def get_page_source(self):
-        self.driver = webdriver.Firefox()
+        socks.set_default_proxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 9050)
+        socket.socket = socks.socksocket
         for i in self.video_urls:
-            self.driver.get(i)
-            self.current_html = self.driver.page_source
-            element = self.driver.find_element_by_xpath('//*[@class="style-scope ytd-page-manager"]')
-            actions = ActionChains(self.driver)
-            actions.move_to_element(element)
-            actions.perform()
-            actions.reset_actions()
-            while True:
-                for j in range(100):
-                    actions.send_keys(Keys.PAGE_DOWN)
-                actions.perform()
-                html = self.driver.page_source
-                if self.current_html != html:
-                    self.current_html=html
-                else:
-                    # self.parse_channel_country()
-                    # self.country_nihongo_true()
-                    # self.channel_subscriber_set()
-                    break
+            html = urllib.request.urlopen(i)
+            self.soup = BeautifulSoup(html, "html.parser")
+            print(self.soup)
 
 
-    def parse_channel_country(self):
-        self.channel_countries = []
-        self.channel_subscribers = []
-        soup = BeautifulSoup(self.current_html, 'html.parser')
-        '''
-        channelSubscriberOfIntExtractionFunction
-        '''
-        channel_subscriber_i = soup.find("yt-formatted-string", class_="style-scope ytd-c4-tabbed-header-renderer")
-        channel_subscriber_lstrip = str(channel_subscriber_i).lstrip('<yt-formatted-string class="style-scope ytd-c4-tabbed-header-renderer" id="subscriber-count">')
-        channel_subscriber_rstrip = channel_subscriber_lstrip.rstrip('</yt-formatted-string>')
-        if "万" in channel_subscriber_rstrip:
-            channel_subscriber_replace = channel_subscriber_rstrip.replace(' ', '')
-            if "." in channel_subscriber_rstrip:
-                s = float(channel_subscriber_rstrip.lstrip('チャンネル登録者数 ').rstrip('万人'))
-                s_i, s_d = str(s).split('.')
-                if 1 is len(s_d):
-                    channel_subscriber_sub = re.sub("\\D", "", str(channel_subscriber_replace))
-                    channel_subscriber_add_million = channel_subscriber_sub + '000'
-                    channel_subscriber_material = int(channel_subscriber_add_million)
-                elif 2 is len(s_d):
-                    channel_subscriber_sub = re.sub("\\D", "", str(channel_subscriber_replace))
-                    channel_subscriber_add_million = channel_subscriber_sub + '00'
-                    channel_subscriber_material = int(channel_subscriber_add_million)
-            else:
-                channel_subscriber_sub = re.sub("\\D", "", str(channel_subscriber_replace))
-                channel_subscriber_add_million = channel_subscriber_sub + '0000'
-                channel_subscriber_material = int(channel_subscriber_add_million)
-        elif "!--css-build:sh" in channel_subscriber_rstrip:
-            channel_subscriber_material = "非表示"
-        else:
-            channel_subscriber_replace = channel_subscriber_rstrip.replace(' ', '')
-            channel_subscriber_sub = re.sub("\\D", "", str(channel_subscriber_replace))
-            channel_subscriber_material = int(channel_subscriber_sub)
-            '''
-            countryOfIntExtractionFunction
-            '''
-        for i in soup.find_all("td", class_="style-scope ytd-channel-about-metadata-renderer"):
-            country_i_findall = re.findall('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">.*</yt-formatted-string>', str(i))
-            country_i_replace = str(country_i_findall).replace('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">', '').replace('</yt-formatted-string>', '')
-            country = str(country_i_replace).replace("['", '').replace("']", '')
-            '''
-            channelSubscriberOfIntExtractionFunction
-            '''
-            channel_subscriber = channel_subscriber_material
-            '''
-            Validation
-            '''
-            if "<!--css-build:shady-->" in str(country):
-                country = None
-            if "<" in str(country):
-                country = None
-            if "[]" in str(country):
-                country = None
-            self.channel_countries.append(country)
-            self.channel_subscribers.append(channel_subscriber)
+    def parse_video_show(self):
+        for i in self.soup.find_all("div", class_="style-scope ytd-video-primary-info-renderer"):
+            print(i)
+            # country_i_findall = re.findall('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">.*</yt-formatted-string>', str(i))
+            # country_i_replace = str(country_i_findall).replace('<yt-formatted-string class="style-scope ytd-channel-about-metadata-renderer">', '').replace('</yt-formatted-string>', '')
+            # country = str(country_i_replace).replace("['", '').replace("']", '')
+            # '''
+            # channelSubscriberOfIntExtractionFunction
+            # '''
+            # channel_subscriber = channel_subscriber_material
+            # '''
+            # Validation
+            # '''
+            # if "<!--css-build:shady-->" in str(country):
+            #     country = None
+            # if "<" in str(country):
+            #     country = None
+            # if "[]" in str(country):
+            #     country = None
+            # self.channel_countries.append(country)
+            # self.channel_subscribers.append(channel_subscriber)
+
+
+    # def parse_tags(self):
+    #     for i in self.soup.re.findall('<meta content="YouTube" property="og:site_name"/>.*<div class="skeleton flexy" id="player">'):
+
 
 
     def country_nihongo_true(self):
